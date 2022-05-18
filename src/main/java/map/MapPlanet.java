92983b74ca72419cb4e11cea0d1495d32c0a37f3
Pierfabio Sanna <pierfabiosanna7@gmail.com>
2022-05-18 22:49:27 +0200

    Add MapPlanet

---
+++ src/main/java/map/MapPlanet.java
@@ -0,0 +1,15 @@
+package map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MapPlanet {
+
+    private int horizontalPosition;
+    private int verticalPosition;
+
+}
