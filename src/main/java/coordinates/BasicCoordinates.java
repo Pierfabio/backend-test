a46fefadd2778f1f81c816be709093509a105c5e
Pierfabio Sanna <pierfabiosanna7@gmail.com>
2022-05-18 22:50:21 +0200

    Add possibility to have constructor w/o parameters

--- src/main/java/coordinates/BasicCoordinates.java
+++ src/main/java/coordinates/BasicCoordinates.java
@@ -2,11 +2,13 @@ package coordinates;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BasicCoordinates {
 
     private int x;
